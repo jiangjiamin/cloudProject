e26294c561e6863ece015df123b58f865120eed3
jiangjiamin <www.jiangjiamin@bosssoft.com.cn>
2020-04-13 21:43:49 +0800

    提交测试

---
+++ src/main/java/cn/com/jiangjiamin/work/WorkApplication.java
@@ -0,0 +1,13 @@
+package cn.com.jiangjiamin.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/com/jiangjiamin/work/WorkApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.jiangjiamin.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
